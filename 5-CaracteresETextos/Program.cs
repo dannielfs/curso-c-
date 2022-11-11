f2d7906f1a9e975f951eb9bc25db5a75f61c8c49
Danniel Sousa <danniel.sousa@gmail.com>
2022-11-11 12:23:22 -0300

    Trabalhando com textos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+
+        Console.WriteLine(letra);
+
+        string nome = "Danniel";
+        Console.WriteLine(nome);
+
+        string primeiraFrase = "Curso ";
+        primeiraFrase = primeiraFrase + 2022;
+        Console.WriteLine(primeiraFrase);
+    }
+}
