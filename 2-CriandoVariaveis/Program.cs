f3c507c174281b5754fdf32cd5300730bb1dc1fa
Danniel Sousa <danniel.sousa@gmail.com>
2022-11-11 11:54:16 -0300

    Criando variaveis

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Ciando Variáveis");
+
+        int idade;
+        idade = 27;
+        Console.WriteLine("Minha idade é " + idade);
+    }
+}
