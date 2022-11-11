2e47fce3076b330bfec58c78dfacf3fd147738e6
Danniel Sousa <danniel.sousa@gmail.com>
2022-11-11 12:00:34 -0300

    Criando variaveis flutuante

---
+++ 3-CriandoVariaveisFlutuante/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Ciando Variáveis Flutuantes");
+
+        double salario;
+        salario = 3000.10;
+        Console.WriteLine("Meu salário é " + salario);
+    }
+}
