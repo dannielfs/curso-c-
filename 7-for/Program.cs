052158c4bba4f77f75a81a6fe8452c7e0d109635
Danniel Sousa <danniel.sousa@gmail.com>
2022-11-11 15:58:39 -0300

    aninhando for

---
+++ 7-for/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Encadeando for");
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
