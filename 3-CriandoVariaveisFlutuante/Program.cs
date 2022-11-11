16bbea7b4c1c008c63ec2e94a1879d6325ee9230
Danniel Sousa <danniel.sousa@gmail.com>
2022-11-11 12:11:05 -0300

    Conversões

--- 3-CriandoVariaveisFlutuante/Program.cs
+++ 3-CriandoVariaveisFlutuante/Program.cs
@@ -6,7 +6,10 @@ class Programa
         Console.WriteLine("Projeto 3 - Ciando Variáveis Flutuantes");
 
         double salario;
+        double comissao;
         salario = 3000.10;
+        comissao = 10.0 * 1.5;
         Console.WriteLine("Meu salário é " + salario);
+        Console.WriteLine("Minha comissão é: " + comissao);
     }
 }
---
+++ 4-Conversoes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Conversões");
+
+        double salario = 3000.15;
+        int salarioInteiro = (int)salario;
+        long milionario = 20000000000000000;
+        short pobre = 1500;
+        float altura = 1.62f;
+    }
+}
